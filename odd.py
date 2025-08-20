4901cb7ea79008ce61fca4fcd86e84235189b99b
Pavel Mysskiy <misik.java@gmail.com>
2025-08-20 20:00:38 +0300

    adding loop and wait
    добавлены повторение в цикле и задержка времени на случайное число в диапазоне

--- odd.py
+++ odd.py
@@ -1,9 +1,15 @@
 from datetime import datetime
+import time
+import random
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19,
         21, 23, 25, 27, 29, 31, 35, 37, 39,
         41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
-right_this_minute = datetime.today().minute
-if right_this_minute in odds:
-    print("Эта минута нечетная.")
-else:
-    print("А это четная минута.")
+for i in range(5):
+    right_this_minute = datetime.today().minute
+    if right_this_minute in odds:
+        print("Эта минута нечетная.")
+    else:
+        print("А это четная минута.")
+    time.sleep(random.randint(1,60))
+
+
